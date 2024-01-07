10783c07e87f285f50dc4666b27563acd2a58638
imsajeel <imsajeel.dev@gmail.com>
2024-01-07 02:06:42 +0000

    data added in product

--- src/components/MenuHome/ProductCard.jsx
+++ src/components/MenuHome/ProductCard.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const ProductCard = ({ product }) => {
   // console.log(product);
   return (
-    <div className="product">
-      <img
-        src={product.image ? product.image : "/images/image-notfound.png"}
-        alt={product.name}
-      />
+    <div className={"product"}>
+      {product.image && (
+        <img
+          src={product.image ? product.image : "/images/image-notfound.png"}
+          alt={product.name}
+        />
+      )}
       <h2>{product.name}</h2>
       <p className="discription">{product.discription}</p>
       <p className="price">&#163; {product.price}</p>
